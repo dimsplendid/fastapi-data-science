75f05b94b3bf9a1874a60af0a721ffbe8fddd7d4
dimsplendid <dimsplendid@gmail.com>
2022-05-14 23:50:58 +0800

    Learn pyhton type hint

---
+++ chapter2_type_hints_02.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+l: list[int] = [1, 2, 3, 4, 5]
+t: tuple[int, str, float] = (1, "hello", 3.14)
+s: set[int] = {1, 2, 3}
+d: dict[str, int] = {"a": 1, "b": 2}
---
+++ chapter2_type_hints_03.py
@@ -0,0 +1,4 @@
+from __future__ import annotations
+from typing import Union
+
+l: list[Union[int, float]] = [1, 2.5, 3.14, 5]
