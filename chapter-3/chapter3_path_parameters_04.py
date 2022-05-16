41b60d243debf6bdbd632cce540fd568c80ead35
dimsplendid <dimsplendid@gmail.com>
2022-05-16 14:49:12 +0800

    Finish path parameters

---
+++ chapter-3/chapter3_path_parameters_04.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Path
+
+app = FastAPI()
+
+
+@app.get('/users/{id}')
+async def get_user(id: int = Path(..., ge=1)):
+    return {'id': id}
+
+# chapter3_path_parameters_05
+# @app.get("/license-plates/{license}")
+# async def get_license_plate(
+#         license: str = Path(..., min_length=9, max_length=9)
+#     ):
+#     return {'license': license}
+
+# chapter3_path_parameters_06
+
+
+@app.get('/license-plates/{license}')
+async def get_license_plate(
+    license: str = Path(..., regex=r"^\w{2}-\d{3}-\w{2}$")
+):
+    return {'license': license}
