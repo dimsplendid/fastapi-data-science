ba2685818bf7d2afd0c08b660b44923917b5a824
dimsplendid <dimsplendid@gmail.com>
2022-05-22 21:05:25 +0800

    Finish optional fields default values

---
+++ chapter-4/chapter4_optional_fields_default_values_01.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class UserProfile(BaseModel):
+    nickname: str
+    location: str | None = None
+    subscribed_newsletter: bool = True
+
+user = UserProfile(nickname="jdoe")
+print(user)
